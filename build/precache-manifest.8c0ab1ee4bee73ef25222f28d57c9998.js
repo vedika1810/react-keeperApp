6be0555d3a62eddc3f9decc93854ea1f572af3f4
vedika1810 <1905437@kiit.ac.in>
2022-02-24 00:58:37 +0530

    second commit

---
+++ build/precache-manifest.8c0ab1ee4bee73ef25222f28d57c9998.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860189b141f567aaad92302b8a4fc702",
+    "url": "/react-keeperApp/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/react-keeperApp/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "f79d542a10746c679db3",
+    "url": "/react-keeperApp/static/js/main.795914d4.chunk.js"
+  },
+  {
+    "revision": "725f6f4ef6d19edb5995",
+    "url": "/react-keeperApp/static/js/runtime-main.adc5c20f.js"
+  }
+]);
